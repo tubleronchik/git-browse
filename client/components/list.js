f3a90a6ed4f6931ade44b22b41c1aefce8f85283
tubleronchik <m.bystramovich@gmail.com>
2020-10-18 17:15:29 +0300

    update list.js

--- client/components/list.js
+++ client/components/list.js
@@ -1,24 +1,44 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios' 
 import { useParams } from 'react-router-dom' 
 import Head from './head'
 
+const Header = (props) => {
+  
 
+}
+const Repo = (props) => {
+  const repo = props
+  return (
+    <div>
+      <div 
+        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-5"
+      >
+        <div key={repo.id} style={{display: "flex"}}>
+          <div style={{width: '250px'}}>{repo.name}</div> 
+        </div>
+      </div>  
+    </div>  
+
+  )
+}
 
 const GettingList = (props) => {
   const [list, setList] = useState([])
   async function getData() {
     const res = await axios.get(`https://api.github.com/users/${props.userName}/repos`)
-    setList(res)
+    setList(res.data)
   } 
   useEffect(() => {  
      getData()
-  }, []) 
+  }, [props.userName]) 
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-teal-500 text-black font-bold rounded-lg border shadow-lg p-10">
-        {list}  
-      </div>
+    <div>
+      {list.map(repo => {
+        return <Repo key={repo.id} {...repo} /> 
+      })}
     </div>
   )
 }
@@ -29,6 +49,7 @@ const List = () => {
     <div>
       <Head title="Hello" />
       <GettingList userName={userName} />
+      <Header userName={userName} />
     </div>
   )
 }
